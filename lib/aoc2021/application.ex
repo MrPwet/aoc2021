0e5507a12be34d13008dda5fc92abefca68ed111
MrPwet <beligat.fx@gmail.com>
2021-11-18 22:03:51 +0100

    Init project

---
+++ lib/aoc2021/application.ex
@@ -0,0 +1,20 @@
+defmodule Aoc2021.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Aoc2021.Worker.start_link(arg)
+      # {Aoc2021.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Aoc2021.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule Aoc2021.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aoc2021,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Aoc2021.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:credo, "~> 1.6", only: [:dev], runtime: false}
+    ]
+  end
+end
